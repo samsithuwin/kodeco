c4ee7058acb3a1f9c6ca112846aeb5b399044952
samsithuwin <150372508+samsithuwin@users.noreply.github.com>
2024-03-15 13:13:24 -0400

    15March 2024 MVP
    Minimal Viable Product

---
+++ Bullseye/BullseyeTests/BullseyeTests.swift
@@ -0,0 +1,30 @@
+import XCTest
+@testable import Bullseye
+
+final class BullseyeTests: XCTestCase {
+  var game: Game!
+  override func setUpWithError() throws {
+    game = Game()
+  }
+  
+  override func tearDownWithError() throws {
+    game = nil
+  }
+  func testScorePositive() {
+    let guess = game.target + 5
+    let score = game.points(sliderValue: guess)
+    XCTAssertEqual(score, 95)
+  }
+  
+  func testScoreNegative() {
+    let guess = game.target - 5
+    let score = game.points(sliderValue: guess)
+    XCTAssertEqual(score, 95)
+  }
+  
+//  func testExample() throws {
+//    XCTAssertEqual(game.points(sliderValue: 50), 999)
+//  }
+  
+  
+}
