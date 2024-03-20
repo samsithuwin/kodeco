f335dfcbd337cb24fb6819727d94ecb1ce766406
samsithuwin <150372508+samsithuwin@users.noreply.github.com>
2024-03-20 12:08:18 -0400

    2Week Bootcamp
    completed

--- SwiftUI-Navigation/Starter/PublicArt/Artwork.swift
+++ SwiftUI-Navigation/Starter/PublicArt/Artwork.swift
@@ -1,39 +1,10 @@
-/// Copyright (c) 2023 Kodeco Inc.
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
+
 
 import MapKit
 import SwiftUI
 
 struct Artwork {
+  let id = UUID()
   let artist: String
   let description: String
   let locationName: String
@@ -42,7 +13,51 @@ struct Artwork {
   let imageName: String
   let coordinate: CLLocationCoordinate2D
   var reaction: String
+  
+//  init(
+//    artist: String,
+//    description: String,
+//    locationName: String,
+//    discipline: String,
+//    title: String,
+//    imageName: String,
+//    coordinate: CLLocationCoordinate2D,
+//    reaction: String
+//  ) 
+  func load() {
+    print(">>>>> Downloading \(self.imageName) <<<<<")
+  }
+//{
+//    print(">>>>> Downloading \(imageName) <<<<<")
+//    self.artist = artist
+//    self.description = description
+//    self.locationName = locationName
+//    self.discipline = discipline
+//    self.title = title
+//    self.imageName = imageName
+//    self.coordinate = coordinate
+//    self.reaction = reaction
+//  }
+
 }
+
+extension Artwork: Identifiable {}
+// //20March2024 Sam Added
+//
+
+
+
+ extension Artwork: Hashable {
+   static func == (lhs: Artwork, rhs: Artwork) -> Bool {
+     lhs.id == rhs.id
+   }
+   
+   func hash(into hasher: inout Hasher) {
+     hasher.combine(id)
+   }
+ }
+
+
 
 // swiftlint:disable line_length
 
@@ -202,3 +217,4 @@ let artData = [
     reaction: "💕")
 ]
 // swiftlint:enable line_length
+
--- SwiftUI-Navigation/Starter/PublicArt/ContentView.swift
+++ SwiftUI-Navigation/Starter/PublicArt/ContentView.swift
@@ -1,44 +1,69 @@
-/// Copyright (c) 2023 Kodeco Inc.
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
+
 
 import SwiftUI
 
 struct ContentView: View {
-  let disciplines = ["statue", "Mural", "plaque"]
-  var body: some View {
-    List(disciplines, id: \.self) { discipline in   Text(discipline)
+  
+  
+  @State private var hideVisited = false
+
+
+  var showArt: [Artwork] {
+    hideVisited ? artworks.filter { $0.reaction.isEmpty } : artworks
+  }
+  
+  private func setReaction(_ reaction: String, for item: Artwork) {
+    self.artworks = artworks.map { artwork in
+      guard artwork.id == item.id else { return artwork }
+      let updateArtwork = Artwork(
+        artist: item.artist,
+        description: item.description,
+        locationName: item.locationName,
+        discipline: item.discipline,
+        title: item.title,
+        imageName: item.imageName,
+        coordinate: item.coordinate,
+        reaction: reaction
+      )
+      return updateArtwork
     }
   }
+
+
+  @State var artworks = artData
+  var body: some View {
+    NavigationStack {
+      List(showArt) { artwork in
+        NavigationLink(value: artwork) {
+          Text("\(artwork.reaction) \(artwork.title)")
+            .onAppear { artwork.load() }
+
+            .contextMenu {
+              Button("Love it:💕") {
+                self.setReaction("💕", for: artwork)
+              }
+              Button("Thoughtful:🙏") {
+                self.setReaction("🙏", for: artwork)
+              }
+              Button("Wow!:🌟 ") {
+                self.setReaction("🌟 ", for: artwork)
+              }
+            }
+        }
+      }
+      .navigationDestination(for: Artwork.self, destination: { artwork in
+        DetailView(artwork: artwork)
+      })
+      .navigationBarTitle("Artworks")
+      .navigationBarItems(
+        trailing: Toggle(isOn: $hideVisited) { Text("Hide Visited") })
+
+      }
+  }
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
